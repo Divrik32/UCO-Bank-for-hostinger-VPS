91f83efc9e48a0c3cb7cb3cb4cc3e81d184fefa2
Divya Mondal <divyamondal2001@gmail.com>
2026-05-27 16:33:21 +0530

    cors changed

--- server/server.js
+++ server/server.js
@@ -15,10 +15,13 @@ connectDB();
 
 // MIDDLEWARES
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173", // frontend URL (Vite)
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "http://localhost:5173", // frontend URL (Vite)
+//   credentials: true
+// }));
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
